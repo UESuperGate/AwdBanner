83e91749cdb689e8b971f52322b0321d919b29f8
mrh929 <809999463@qq.com>
2021-04-04 17:13:22 +0800

    shorten the code

--- ptrace_demo.c
+++ ptrace_demo.c
@@ -8,48 +8,46 @@
 #include <sys/user.h>
 #include <unistd.h>
 
-void server() {
-  ptrace(PTRACE_TRACEME, 0, 0, 0);
-  raise(SIGCONT);
+void sandbox(){
+	int status;
+	struct user_regs_struct regs;
+	pid_t pid = fork();
+	if (pid < 0) {
+		printf("server init failed!\n");
+		exit(1);
+	}
 
-  //system("ls");
-  open("fuc");
-  printf("aa");
-  
-  return;
-}
+	if (!pid){
+		ptrace(PTRACE_TRACEME, 0, 0, 0);
+		raise(SIGCONT);
+		return;
+	}
 
-void daemonn(pid_t child_pid) {
-  int status;
-  struct user_regs_struct regs;
+	waitpid(pid, &status, 0);
+	while(WIFSTOPPED(status)) {
+		ptrace(PTRACE_SYSCALL, pid, 0, 0);
+		waitpid(pid, &status, 0);
+		ptrace(PTRACE_GETREGS, pid, 0, &regs);
+		printf("syscall:%d\n", regs.orig_rax);
 
-  waitpid(child_pid, &status, 0);
-  while(WIFSTOPPED(status)) {
-    ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
-    waitpid(child_pid, &status, 0);
-    ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
-    printf("syscall:%d\n", regs.orig_rax);
-
-    if(regs.orig_rax == SYS_open || \
-        regs.orig_rax == SYS_fork || \
-        regs.orig_rax == SYS_clone || \
-        regs.orig_rax == SYS_vfork) {
-      regs.orig_rax = SYS_exit_group;
-      regs.rdi = 0;
-      ptrace(PTRACE_SETREGS, child_pid, 0, &regs);
-      puts("execve syscall detected!");
-    }
-  }
+		if(regs.orig_rax == SYS_open || \
+			regs.orig_rax == SYS_fork || \
+			regs.orig_rax == SYS_clone || \
+			regs.orig_rax == SYS_vfork) {
+				regs.orig_rax = SYS_exit_group;
+				regs.rdi = 0;
+				ptrace(PTRACE_SETREGS, pid, 0, &regs);
+				puts("execve syscall detected!");
+				exit(0);
+			}
+		}
 }
 
 int main() {
-  pid_t pid = fork();
-  if (pid < 0) {
-    printf("server init failed!\n");
-    exit(1);
-  }
+	sandbox();
 
-  if (!pid) server();
-  else daemonn(pid);
-  return 0;
+	// system("ls");
+	open("fuc");
+	printf("\n");
+	return 0;
 }
