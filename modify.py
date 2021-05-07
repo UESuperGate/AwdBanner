70eb134467c0072e9dddd05997eba7959d82def0
mrh929 <809999463@qq.com>
2021-05-08 00:45:42 +0800

    rewrite logic of modify.py

--- modify.py
+++ modify.py
@@ -30,41 +30,47 @@ def main():
         shellcode_path = sys.argv[2]
 
     binary = lief.parse(binary_path)
-    main_address = binary.header.entrypoint
+    start_address = binary.header.entrypoint
     physical_address = add_segment(binary)
+    binary.header.entrypoint = physical_address # change the entrypoint to start_address
     logging.debug("new_segment address: " + hex(physical_address))
 
-    content = binary.get_content_from_virtual_address(main_address, 0x20)
-    print([hex(each) for each in content])
+    # content = binary.get_content_from_virtual_address(start_address, 0x20)
+    # print([hex(each) for each in content])
 
-    code = struct.pack('B'*0x20, *content)
-    md = Cs(CS_ARCH_X86, CS_MODE_64)
-
-    patch_start = main_address
-    patch_end = patch_start
-    for (address, size, mnemonic, op_str) in md.disasm_lite(code, 0):
-        print("0x%x 0x%x:\t%s\t%s" %(address, address + main_address, mnemonic, op_str))
-        if address > 4:
-            patch_end += address - 1
-            logging.debug("patch_start = 0x%x" % patch_start)
-            logging.debug("patch_end = 0x%x" % patch_end)
-            logging.debug("patch_length = 0x%x" % (patch_end-patch_start+1))
-            break
+    # code = struct.pack('B'*0x20, *content)
+    # md = Cs(CS_ARCH_X86, CS_MODE_64)
+    # patch_start = start_address
+    # patch_end = patch_start
+    # for (address, size, mnemonic, op_str) in md.disasm_lite(code, 0):
+    #     print("0x%x 0x%x:\t%s\t%s" %(address, address + start_address, mnemonic, op_str))
+    #     if address > 4:
+    #         patch_end += address - 1
+    #         logging.debug("patch_start = 0x%x" % patch_start)
+    #         logging.debug("patch_end = 0x%x" % patch_end)
+    #         logging.debug("patch_length = 0x%x" % (patch_end-patch_start+1))
+    #         break
     
-    assert(patch_start != patch_end)
+    # assert(patch_start != patch_end)
 
-    jmp_shellcode_content = asm("jmp 0x%x" % physical_address, vma=patch_start) 
-    jmp_shellcode_code = list(struct.unpack('B'*len(jmp_shellcode_content), jmp_shellcode_content))  # assembly "jmp shellcode"    
-    binary.patch_address(patch_start, jmp_shellcode_code)
+    # jmp_shellcode_content = asm("jmp 0x%x" % physical_address, vma=patch_start) 
+    # jmp_shellcode_code = list(struct.unpack('B'*len(jmp_shellcode_content), jmp_shellcode_content))  # assembly "jmp shellcode"    
+    # binary.patch_address(patch_start, jmp_shellcode_code)
 
 
     with open(shellcode_path, "rb") as f:
         shellcode = [each for each in f.read()]
 
-    main_code = content[:(patch_end-patch_start+1)] # assembly replaced by "jmp shellcode"
-    ret_content = asm("push 0x%x" % (patch_end + 1)) 
+    # main_code = content[:(patch_end-patch_start+1)] # assembly replaced by "jmp shellcode"
+    ret_content = asm("""
+        call $+5
+        pop rax
+        sub rax, 0x5
+        sub rax, {}
+        push rax
+    """.format(hex(physical_address-start_address)), arch="amd64") 
     ret_code = list(struct.unpack('B'*len(ret_content), ret_content))  # assembly "jmp main+0xnn"
-    shellcode = main_code + ret_code + shellcode
+    shellcode = ret_code + shellcode
     binary.patch_address(physical_address, shellcode)
 
 
