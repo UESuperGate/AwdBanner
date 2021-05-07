b897fa0c2c574e3d23e053b0c2080ef398d0cea6
mrh929 <809999463@qq.com>
2021-05-07 23:11:01 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@
 binary_path="$1"
 arch="x64"
 
-if [ ! -f "$1" ]; then
+if [ ! -f "binary_path" ]; then
     echo "Usage: ./build.sh binary"
     exit
 fi
 
-gcc -c  ${arch}_entry.s banner.c -fno-stack-protector
+gcc -c ${arch}_entry.s banner.c -fno-stack-protector
 ld ${arch}_entry.o banner.o
 objcopy -O binary -j .text a.out shellcode
 rm a.out
 rm *.o
 
-python modify.py ${binary_path} shellcode
+python modify.py "${binary_path}" shellcode
 chmod +x "${binary_path}.patched"
