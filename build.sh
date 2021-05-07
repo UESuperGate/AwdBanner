7bfa09c1ec53024ca2c1694a1e269bb8afa76419
mrh929 <809999463@qq.com>
2021-05-07 22:54:32 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
+binary_path="$1"
+
+if [ ! -f "$1" ]; then
+    echo "Usage: ./build.sh binary"
+    exit
+fi
+
 gcc -c  awdDefender.s ptrace_demo.c -fno-stack-protector
 ld awdDefender.o ptrace_demo.o
 objcopy -O binary -j .text a.out shellcode
 rm a.out
-rm *.o 
+rm *.o
+
+python modify.py ${binary_path} shellcode
+chmod +x "${binary_path}.patched"
