72f1e48e7c251ef5b7628f4bbe04cb2c2fb683a7
mrh929 <809999463@qq.com>
2021-06-21 19:24:12 +0800

    重构modify.py,使用手写shellcode

---
+++ old/version2/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+binary_path="$1"
+arch="x64"
+
+if [ ! -f "${binary_path}" ]; then
+    echo "Usage: ./build.sh binary"
+    exit
+fi
+
+gcc -c ${arch}_entry.s banner.c -fno-stack-protector
+ld ${arch}_entry.o banner.o
+objcopy -O binary -j .text a.out shellcode
+rm a.out
+rm *.o
+
+python modify.py "${binary_path}" shellcode
+chmod +x "${binary_path}.patched"
